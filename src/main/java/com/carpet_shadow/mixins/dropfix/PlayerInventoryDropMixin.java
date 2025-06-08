28c8665abe83205dafd664d179e09dfe2d871d0a
js-12345 <schaeferjonathan@gmx.net>
2025-06-08 18:19:37 +0200

    fixing duplication and shadow item deletion glitch on shadow item pickup

--- src/main/java/com/carpet_shadow/mixins/dropfix/PlayerInventoryDropMixin.java
+++ src/main/java/com/carpet_shadow/mixins/dropfix/PlayerInventoryDropMixin.java
@@ -3,14 +3,28 @@ package com.carpet_shadow.mixins.dropfix;
 import com.carpet_shadow.CarpetShadow;
 import com.carpet_shadow.CarpetShadowSettings;
 import com.carpet_shadow.interfaces.ShadowItem;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(PlayerInventory.class)
-public abstract class PlayerInventoryDropMixin {
+public class PlayerInventoryDropMixin {
+
+    @ModifyReturnValue(method = "canStackAddMore(Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)Z", at = @At("RETURN"))
+    public boolean canStackAddMore(boolean original, ItemStack existingStack, ItemStack stack) {
+        if (CarpetShadowSettings.shadowItemDropFix) {
+            if (((ShadowItem) (Object) stack).carpet_shadow$getShadowId() != null) {
+                CarpetShadow.LOGGER.warn("prevent inventory combine");
+                return false;
+            }
+        }
+
+        return original;
+    }
 
     @WrapMethod(method = "dropSelectedItem")
     public ItemStack fixDrop(boolean entireStack, Operation<ItemStack> original) {
